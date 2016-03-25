096f863f107092a7db2e26e5595d15aea0bc4825
sclgcj <sclgcj@163.com>
2016-03-25 14:04:49 +0800

    add manage dispose files

---
+++ src/comm/st_manage_dispose.h
@@ -0,0 +1,12 @@
+#ifndef ST_MANAGE_DISPOSE_H
+#define ST_MANAGE_DISPOSE_H 1
+
+#include "st_manage.h"
+
+int
+st_manager_add_dispose_node(
+	struct list_head *pStruNode,	
+	STHandle				 struHandle
+);
+
+#endif
