09d293a09a8152c755b5acea79b574c06cbc9228
sclgcj <sclgcj@163.com>
2016-03-29 16:01:23 +0800

    add client_data's manage file

---
+++ src/comm/ml_manage_client_data.h
@@ -0,0 +1,20 @@
+#ifndef ML_MANAGE_CLIENT_DATA_H
+#define ML_MANAGE_CLIENT_DATA_H 1
+
+#include "ml_manage.h"
+
+int
+ml_manager_get_client_data(
+	int			 iID,
+	MLHandle struHandle,
+	void		 **pUserData
+);
+
+int
+ml_manager_add_client_data(
+	void				 *pUserData,
+	MLDataHandle struHandle,
+	int					 *piID
+);
+
+#endif
