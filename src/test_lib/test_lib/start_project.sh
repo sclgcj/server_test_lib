4b5d8f486979a26108ceb6da764fd24e2c0a23ef
sclgcj <sclgcj@163.com>
2016-08-09 07:22:30 +0800

    增加makefile在删除和添加新项目的时候对last_proj和proj_conf的操作，把sed命令的的匹配方式修改为全词匹配；取消start_project中的命令行提出，更改为直接读取proj_conf和last_proj.txt文件来获取最近使用的项目

--- src/test_lib/test_lib/start_project.sh
+++ src/test_lib/test_lib/start_project.sh
@@ -4,63 +4,32 @@ new_file=""
 proj=""
 num=""
 
+
 if [ ""$1 != "" ]
 then
 	proj=$1
 else
 	if [ -e ./last_proj.txt ]
 	then
-		proj=`cat ./last_proj.txt | tr '\n' '\0'`
-	else
-		echo "erer"
-		if [ ! -e "./proj_conf" ]
-		then
-			echo "没有创建新项目，请使用make new proj=项目创建新项目"
-			exit
-		fi
-		proj_dir=`cat proj_conf | tr '\n' ' '`
-		flag=0
-		echo "flsg = "$flag
-		echo "proj_dir = $proj_dir"
-		while [ $flag -eq 0 ]
-		do
-			echo "请选择想要处理的工程序号:"
-			for i in $proj_dir
-			do
-				echo "1 $i"
-			done
-			read num
-			cmt=1
-			for i in $proj_dir
-			do
-				if [ ""$cmt == $num ]
-				then
-					echo "erer"
-					flag=1
-					proj=$i
-					break;
-				fi
-				let "cmt+=1"
-			done
-			echo "flag = $flag"
-			if [ $flag -eq 1 ]
-			then
-				break
-			fi
-			echo "错误选项，请选择正确的序号!"
-			echo ""
-			echo ""
-			continue
-		done
+		all_file=`cat ./last_proj.txt | tr '\n' ' '`
+		@echo "all_file = $all_file"
+		proj=`tail -n 1 ./last_proj.txt | tr '\n' '\0'`
+	else 
+		proj=`tail -n 1 ./proj_conf | tr '\n' '\0'`
 	fi
-
 fi
 
-echo $proj > ./last_proj.txt
-files=`ls -lt ./$proj/$proj/ | awk '{print $9}'`
-echo "files = $files"
+echo "proj = $proj"
 
-echo $files
+if [ -e ./last_proj.txt ]
+then
+	sed -i "/\<$proj\>/d" last_proj.txt
+	echo $proj >> ./last_proj.txt
+else
+	echo $proj > ./last_proj.txt
+fi
+files=`ls -lt ./$proj/$proj/*.[ch] | awk '{print $9}' | tr '\n' ' '`
+#echo "files = $files"
 
 
 find `pwd` -name "*.[ch]" -o -name "*.cpp" > cscope.files  
@@ -79,9 +48,9 @@ do
 	break
 done
 
-echo "newfile = $proj/$proj/$new_file"
+echo "newfile = $new_file"
 
 
-vim $proj/$proj/$new_file
+vim $new_file
 
 echo hhhh
