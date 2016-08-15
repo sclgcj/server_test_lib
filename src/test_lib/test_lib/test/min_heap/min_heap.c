ba58b62b108708f1010e1f8d56900c3196629239
sclgcj <sclgcj@163.com>
2016-08-15 16:18:24 +0800

    修改对测试程序

--- src/test_lib/test_lib/test/min_heap/min_heap.c
+++ src/test_lib/test_lib/test/min_heap/min_heap.c
@@ -1,23 +1,28 @@
 #include "tc_comm.h"
 #include "tc_heap.h"
 
+struct tc_test {
+	int val;
+	unsigned long heap_data;
+};
+
 static int
 tc_test_cmp(
 	unsigned long new,
 	unsigned long old
 )
 {
-	int new_val = 0, old_val = 0;
+	struct tc_test *new_val = NULL, *old_val = NULL;
 
-	printf("address = %p, %p--00\n", (char*)new, (char*)old);
-	new_val = *((int*)new);
-	old_val = *((int*)old);
+	//printf("address = %p, %p--00\n", (char*)new, (char*)old);
+	new_val = ((struct tc_test*)new);
+	old_val = ((struct tc_test*)old);
+//	printf("val = %d, %d\n", new_val->val, old_val->val);
 
-	if (new_val > old_val)
-		return TC_HEAP_LARGER;
-	else if (new_val == old_val)
-		return TC_HEAP_LESS;
-	return TC_HEAP_SAME;
+	if (new_val->val < old_val->val)
+		return 0;
+
+	return -1;
 }
 
 static void
@@ -28,6 +33,15 @@ tc_test_print(
 	printf("%d ", *((int*)user_data));
 }
 
+static void
+tc_test_destroy(
+	unsigned long user_data
+)
+{
+	struct tc_test *tt = (struct tc_test *)user_data;
+
+	return;
+}
 
 #define ARRAY_SIZE 100
 int 
@@ -39,9 +53,10 @@ main(
 	int i = 0;
 	int num = 0;
 	int limit_num = 0;
-	int *array = NULL;
+	struct tc_test *array = NULL;
 	unsigned long user_data = 0;
 	tc_heap_handle_t handle;
+	struct timespec t1, t2;
 
 	srand(time(NULL));
 
@@ -49,29 +64,55 @@ main(
 
 	limit_num = num * 10;
 	printf("num = %d\n", num);
-	array = (int*)calloc(num, sizeof(int));
+	array = (struct tc_test*)calloc(num, sizeof(struct tc_test));
 
 	handle = tc_heap_create(tc_test_cmp);
 
+
+	clock_gettime(CLOCK_MONOTONIC, &t1);
 	for (; i < num; i++) {
-		array[i] = random() % num * limit_num;
-		printf("%d ", array[i]);
+		array[i].val = random() % limit_num + 60;
+		printf("%d ", array[i].val);
 	}
 	printf("\n");
+	clock_gettime(CLOCK_MONOTONIC, &t2);
+	fprintf(stderr, "t2 - t1 = %ld.%ld\n", t2.tv_sec - t1.tv_sec, t2.tv_nsec, t1.tv_nsec);
 
-	for (i = 0; i < num; i++) 
-		tc_heap_node_add(handle, (unsigned long)&array[i]);
+	clock_gettime(CLOCK_MONOTONIC, &t1);
+	for (i = 0; i < num; i++) {
+		array[i].heap_data = tc_heap_node_add(handle, (unsigned long)&array[i]);
+	}
+	clock_gettime(CLOCK_MONOTONIC, &t2);
+	fprintf(stderr, "t2 - t1 = %ld.%ld\n", t2.tv_sec - t1.tv_sec, t2.tv_nsec, t1.tv_nsec);
 
 	tc_heap_traversal(handle, tc_test_print);
+	printf("\n");
 
-	/*for (i = 0; i < 10; i++) {
+	//for (i = 0; i < num; i++) {
+		clock_gettime(CLOCK_MONOTONIC, &t1);
 		tc_heap_root_data_get(handle, &user_data);
-		*((int*)user_data) += 60;
-		tc_heap_head_node_del(handle);
+		clock_gettime(CLOCK_MONOTONIC, &t2);
+		fprintf(stderr, "t2 - t1 = %ld.%ld\n", t2.tv_sec - t1.tv_sec, t2.tv_nsec, t1.tv_nsec);
+		
+		tc_heap_traversal(handle, tc_test_print);
+		((struct tc_test*)user_data)->val -= 60;
+		//printf("value = %d\n", ((struct tc_test*)user_data)->val);
+		clock_gettime(CLOCK_MONOTONIC, &t1);
+		tc_heap_node_add(handle, user_data);
+		clock_gettime(CLOCK_MONOTONIC, &t2);
+		fprintf(stderr, "t2 - t1 = %ld.%ld\n", t2.tv_sec - t1.tv_sec, t2.tv_nsec, t1.tv_nsec);
+		//tc_heap_node_adjust(handle, ((struct tc_test*)user_data)->heap_data);
 		printf("====================start=========================\n");
 		tc_heap_traversal(handle, tc_test_print);
 		printf("\n");
 		printf("==================== end =========================\n");
-	}*/
+//	}
+
+	fprintf(stderr, "hhhhhhhhhhhhhhhhh\n");
+	sleep(10);
+	tc_heap_destroy(handle, NULL);//tc_test_destroy);
+	fprintf(stderr, "see result\n");
+	sleep(20);
+	return 0;
 }
 
--- src/test_lib/test_lib/test/min_heap/tc_heap.c
+++ src/test_lib/test_lib/test/min_heap/tc_heap.c
@@ -9,7 +9,11 @@
  * 该模块类似于一个优先级队列，采用的是最小堆的实现方法，目的在于
  * 帮助简化定时器的检测。当前的定时器实现，是检测所有的节点，这样
  * 子虽然目前满足需求，但是总感觉不是很好，因此参考了libevent的介
- * 绍，思考之后决定尝试使用最小堆。
+ * 绍，思考之后决定尝试使用最小堆。经过测试，2016-8-15 的版本，在
+ * 对100万的数据进行处理的时候，插入删除的效率感觉还行，基本上都在
+ * 1s之内完成了操作，但是感觉还是不是很满意。 在性能比较好的测试机
+ * 上进行测试，1000W的处理效率阔以保持在2s以内，这还是让人感觉非常
+ * 兴奋，看来是开始的处理时间是因为虚拟机的本身的性能比较低造成的。
  */
 struct tc_heap_node {
 	unsigned long user_data;
@@ -18,7 +22,6 @@ struct tc_heap_node {
 };
 
 struct tc_heap_head {
-	struct tc_heap_node *last_leaf;
 	struct tc_heap_node *last;
 	struct tc_heap_node *root;
 };
@@ -27,7 +30,6 @@ struct tc_heap_data {
 	int (*user_cmp_func)(unsigned long new, unsigned long old);
 	struct list_head head;
 	pthread_mutex_t heap_mutex;
-	pthread_mutex_t heap_list_mutex;
 	struct tc_heap_head heap_head;
 };
 
@@ -38,11 +40,10 @@ tc_heap_default_cmp(
 	unsigned long old
 )
 {
+	//默认采用最小堆
 	if (new > old)
-		return TC_HEAP_LARGER;
-	if (new == old)
-		return TC_HEAP_SAME;
-	return TC_HEAP_LESS;
+		return TC_OK;
+	return TC_ERR;
 }
 
 tc_heap_handle_t
@@ -54,12 +55,11 @@ tc_heap_create(
 
 	heap_data = (struct tc_heap_data *)calloc(1, sizeof(*heap_data));
 	if (!heap_data) {
-		TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
+		//TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
 		return NULL;
 	}
 	INIT_LIST_HEAD(&heap_data->head);
 	pthread_mutex_init(&heap_data->heap_mutex, NULL);
-	pthread_mutex_init(&heap_data->heap_list_mutex, NULL);
 	if (user_cmp_func)
 		heap_data->user_cmp_func = user_cmp_func;
 	else
@@ -70,7 +70,7 @@ tc_heap_create(
 	if (!heap_data->heap_head.root) {
 		pthread_mutex_destroy(&heap_data->heap_mutex);
 		TC_FREE(heap_data);
-		TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
+		//TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
 		return NULL;
 	}
 	list_add_tail(&heap_data->heap_head.root->node, &heap_data->head);
@@ -103,11 +103,10 @@ tc_heap_add_adjust(
 	if (!heap_node->parent)
 		return;
 
-	pthread_mutex_lock(&heap_data->heap_mutex);
 	while(tmp != heap_data->heap_head.root) {
 		if (tmp->parent) {
 			ret = heap_data->user_cmp_func(tmp->user_data, tmp->parent->user_data);
-			if (ret != TC_HEAP_LARGER) {
+			if (ret != TC_OK) {
 				tc_heap_swap_val(tmp, tmp->parent);
 				tmp = tmp->parent;
 				continue;
@@ -115,22 +114,18 @@ tc_heap_add_adjust(
 		}
 		break;
 	}
-	pthread_mutex_unlock(&heap_data->heap_mutex);
-
-	//printf("--------------------------------------------\n");
-	//tc_heap_traversal((tc_heap_handle_t)heap_data, NULL);
-	//printf("--------------------------------------------\n");
 }
 
 static void
 tc_heap_del_adjust(
-	struct tc_heap_data *heap_data
+	struct tc_heap_data *heap_data,
+	struct tc_heap_node *root
 )
 {
 	int ret = 0;
 	struct tc_heap_node *tmp = NULL, *child = NULL;
 
-	tmp = heap_data->heap_head.root;
+	tmp = root;
 	
 	while (tmp->right || tmp->left) {
 		if (tmp->right && tmp->left && 
@@ -138,7 +133,7 @@ tc_heap_del_adjust(
 			ret = heap_data->user_cmp_func(
 					tmp->right->user_data, 
 					tmp->left->user_data);
-			if (ret == TC_HEAP_LARGER)
+			if (ret == TC_OK)
 				child = tmp->left;
 			else 
 				child = tmp->right;
@@ -149,117 +144,102 @@ tc_heap_del_adjust(
 			else
 				child = tmp->left;
 		}
-		PRINT("address = %p, %p\n", (char*)tmp->user_data, (char*)tmp->user_data);
+		if (child->user_data == -1)
+			break;
 		ret = heap_data->user_cmp_func(tmp->user_data, child->user_data);
-		//if (ret != TC_HEAP_LARGER)
-		//:	return;
-		PRINT("\n");
-		if (ret >= TC_HEAP_LARGER) {
+		if (ret == TC_OK) {
 			tc_heap_swap_val(tmp, child);
 			tmp = child;
+			continue;
 		}
+		break;
 	}
 }
 
-int
+unsigned long
 tc_heap_node_add(
 	tc_heap_handle_t handle,
 	unsigned long    user_data
 )
 {
+	unsigned long ret = 0;
 	struct tc_heap_data *heap_data = NULL;
 	struct tc_heap_node *right_node = NULL, *left_node = NULL, *heap_node = NULL;
 
 	if (!handle) {
-		TC_ERRNO_SET(TC_PARAM_ERROR);
+		//TC_ERRNO_SET(TC_PARAM_ERROR);
 		return TC_OK;
 	}
 
 	heap_data = (struct tc_heap_data*)handle;
 
-	pthread_mutex_lock(&heap_data->heap_list_mutex);
+	pthread_mutex_lock(&heap_data->heap_mutex);
 	heap_node = list_entry(heap_data->head.next, struct tc_heap_node, node);
 	list_del_init(&heap_node->node);	
-	PRINT("\n");
 	if (heap_node->user_data == (unsigned long)-1) {
 		heap_node->user_data = user_data;
+		heap_data->heap_head.last = heap_node;
 		list_add_tail(&heap_node->node, &heap_data->head);
-		pthread_mutex_unlock(&heap_data->heap_list_mutex);
 		tc_heap_add_adjust(heap_data, heap_node);
-		return TC_OK;
-	}
-	pthread_mutex_unlock(&heap_data->heap_list_mutex);
+		goto out;
+	} 
 
-	PRINT("\n");
 	left_node = (struct tc_heap_node *)calloc(1, sizeof(*left_node));
 	if (!left_node) {
-		TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
-		return TC_ERR;
+		//TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
+		goto err;
 	}
 	left_node->user_data = user_data;
 	left_node->parent = heap_node;
 	heap_node->left = left_node;
+	heap_data->heap_head.last = left_node;
 	tc_heap_add_adjust(heap_data, left_node);
 	right_node = (struct tc_heap_node *)calloc(1, sizeof(*right_node));
 	if (!right_node) {
-		TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
-		return TC_ERR;
+		//TC_ERRNO_SET(TC_NOT_ENOUGH_MEMORY);
+		goto err;
 	}
 	right_node->parent = heap_node;
 	right_node->user_data = (unsigned long)-1;
 	heap_node->right = right_node;
-	PRINT("\n");
-	pthread_mutex_lock(&heap_data->heap_list_mutex);
 	list_add_tail(&left_node->node, &heap_data->head);
 	list_add(&right_node->node, &heap_data->head);
-	pthread_mutex_unlock(&heap_data->heap_list_mutex);
-	/*pthread_mutex_lock(&heap_data->heap_mutex);
-	if (heap_data->heap_head.last == NULL) {
-		heap_data->heap_head.last = heap_node;
-		heap_data->heap_head.root = heap_node;
-	} else {
-		heap_node->parent = heap_data->heap_head.last;
-		if (!heap_data->heap_head.last->left) 
-			heap_data->heap_head.last->left = heap_node;
-		else {
-			heap_data->heap_head.last->right = heap_node;
-			if (!heap_data->heap_head.last->parent) 
-				heap_data->heap_head.last = 
-					heap_data->heap_head.last->left;
-			else if (heap_data->heap_head.last->parent->right == 
-					heap_data->heap_head.last)
-				heap_data->heap_head.last = 
-					heap_data->heap_head.last->parent->left->left;
-			else 
-				heap_data->heap_head.last = 
-					heap_data->heap_head.last->parent->right;
-		}
-		tc_heap_add_adjust(heap_data, heap_node);
-	}
-	pthread_mutex_unlock(&heap_data->heap_mutex);*/
-
+	ret = TC_OK;
+	goto out;
+err:
+	ret = TC_ERR;
+out:
+	pthread_mutex_unlock(&heap_data->heap_mutex);
 	return TC_OK;
 }
 
-int
-tc_heap_head_node_del(
-	tc_heap_handle_t handle
+static void
+tc_heap_node_del( struct tc_heap_data *heap_data
 )
 {
-	struct tc_heap_data *heap_data = NULL;
+	struct tc_heap_node *heap_node = NULL, *last = NULL;
 
-	if (!handle) {
-		TC_ERRNO_SET(TC_PARAM_ERROR);
-		return TC_ERR;
-	}
+	last = heap_data->heap_head.last;
+	heap_node = heap_data->heap_head.root;
+	if (!heap_data->heap_head.last) 
+		return;
+	//交换根结点和最后一个结点的值
+	tc_heap_swap_val(last, heap_node);	
+	heap_data->heap_head.last->user_data = (unsigned long)-1;
 
-	heap_data = (struct tc_heap_data *)handle;
+	// 把最后一个结点从添加队列中删除，此时它应该是最末尾的一个，
+	// 然后再添加到添加队列的头，这样它就会被第一个处理，处理
+	// 完之后会被再次添加进队列尾部
+	list_del_init(&last->node);
+	list_add(&last->node, &heap_data->head);
 
-	pthread_mutex_lock(&heap_data->heap_mutex);
-	tc_heap_del_adjust(heap_data);
-	pthread_mutex_unlock(&heap_data->heap_mutex);
+	//将last结点指向最后结点的上一个结点。
+	if (last == last->parent->right)
+		heap_data->heap_head.last = last->parent->left;
+	else
+		heap_data->heap_head.last = last->parent;
 
-	return TC_OK;
+	tc_heap_del_adjust(heap_data, heap_node);
 }
 
 int
@@ -271,18 +251,19 @@ tc_heap_root_data_get(
 	struct tc_heap_data *heap_data = NULL;
 
 	if (!handle || !user_data) {
-		TC_ERRNO_SET(TC_PARAM_ERROR);
+		//TC_ERRNO_SET(TC_PARAM_ERROR);
 		return TC_ERR;
 	}
 
 	heap_data = (struct tc_heap_data *)handle;
 	if (!heap_data->heap_head.root) {
-		TC_ERRNO_SET(TC_NO_HEAP_DATA);
+		//TC_ERRNO_SET(TC_NO_HEAP_DATA);
 		return TC_ERR;
 	}
 
 	pthread_mutex_lock(&heap_data->heap_mutex);
 	(*user_data) = heap_data->heap_head.root->user_data;
+	tc_heap_node_del(handle);
 	pthread_mutex_unlock(&heap_data->heap_mutex);
 
 	return TC_OK;
@@ -307,9 +288,8 @@ tc_heap_traversal(
 	struct tc_heap_data *heap_data = NULL;
 	struct tc_heap_node *tmp_left = NULL, *tmp_right = NULL, *tmp = NULL;
 
-	PRINT("\n");
 	if (!handle) {
-		TC_ERRNO_SET(TC_PARAM_ERROR);
+		//TC_ERRNO_SET(TC_PARAM_ERROR);
 		return TC_ERR;
 	}
 	if (heap_traversal)
@@ -318,17 +298,15 @@ tc_heap_traversal(
 		tmp_traversal = tc_heap_default_traversal;
 	
 	heap_data = (struct tc_heap_data *)handle;
-	PRINT("\n");
 	tmp = heap_data->heap_head.root;
 	if (!tmp)
 		return TC_OK;
 
-	PRINT("\n");
 	INIT_LIST_HEAD(&head);
 	list_add_tail(&tmp->traversal_node, &head);
 
 	while (!list_empty(&head)) {
-		PRINT("\n");
+//		PRINT("\n");
 		tmp = list_entry(head.next, struct tc_heap_node, traversal_node);
 		list_del_init(head.next);
 		if (tmp->user_data == (unsigned long)-1)
@@ -338,6 +316,53 @@ tc_heap_traversal(
 			list_add_tail(&tmp->left->traversal_node, &head);
 		if (tmp->right)
 			list_add_tail(&tmp->right->traversal_node, &head);
+	}
+
+	return TC_OK;
+}
+
+int
+tc_heap_destroy(
+	tc_heap_handle_t handle,
+	void (*user_data_destroy)(unsigned long user_data)
+)
+{
+	int count = 0;
+	void (*tmp_traversal)(unsigned long user_data);
+	struct list_head head;
+	struct tc_heap_data *heap_data = NULL;
+	struct tc_heap_node *tmp_left = NULL, *tmp_right = NULL, *tmp = NULL;
+
+	if (!handle) {
+		//TC_ERRNO_SET(TC_PARAM_ERROR);
+		return TC_ERR;
+	}
+
+	heap_data = (struct tc_heap_data *)handle;
+	tmp = heap_data->heap_head.root;
+	if (!tmp)
+		return TC_OK;
+
+	INIT_LIST_HEAD(&head);
+	list_add_tail(&tmp->traversal_node, &head);
+
+	while (!list_empty(&head)) {
+//		PRINT("\n");
+		tmp = list_entry(head.next, struct tc_heap_node, traversal_node);
+		list_del_init(head.next);
+		if (tmp->user_data == (unsigned long)-1) {
+			TC_FREE(tmp);
+			break;
+		}
+		if (user_data_destroy)
+			user_data_destroy(tmp->user_data);
+		if (tmp->left) 
+			list_add_tail(&tmp->left->traversal_node, &head);
+		if (tmp->right)
+			list_add_tail(&tmp->right->traversal_node, &head);
+		tmp->right = NULL;
+		tmp->left = NULL;
+		TC_FREE(tmp);
 	}
 
 	return TC_OK;
