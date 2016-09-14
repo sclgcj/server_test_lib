ab54f69d6f991856cc62a48c87417958c169600e
sclgcj <sclgcj@163.com>
2016-09-14 18:24:11 +0800

    增加test_client模板

---
+++ src/test_lib/test_lib/project_model/test_client/proj_test_model.h
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tc_comm.h"
+#include "tc_err.h"
+#include "tc_cmd.h"
+#include "tc_init.h"
+#include "tc_print.h"
+#include "tc_config.h"
+#include "tc_config.h"
+#include "tc_create.h"
+#include "tc_hub.h"
+#include "tc_interface.h"
+#include "tc_json.h"
+
+
